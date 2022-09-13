09d36a7a5ced8cc8126e278ab656fd65ef797b69
Orlando <carlos.orlando.rios@gmail.com>
2022-09-13 13:48:39 -0500

    couldn't get slideshow to show on browser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css'
-import { filmData } from './film-data'
+import Slideshow from './Slideshow.js'
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [data, setData] = useState(null);
+  const getData = () => {
+    fetch('film-data.json'
+      , {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setData(myJson)
+      });
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="slides-app">
-      <h1>Your Slideshow</h1>
+      <h1 className="title">Ghibli</h1>
+      {data ? <Slideshow films={data} /> : <div></div>}
     </div>
   )
 }
